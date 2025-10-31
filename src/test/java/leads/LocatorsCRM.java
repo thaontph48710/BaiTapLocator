7f324098de1b2b14eeb3505bc27492b004b36f61
ThanhThaoTest <theooonguyenthanh@gmail.com>
2025-10-31 07:16:40 +0700

    Sua bai xpath module Leads

--- src/test/java/leads/LocatorsCRM.java
+++ src/test/java/leads/LocatorsCRM.java
@@ -3,16 +3,17 @@ package leads;
 public class LocatorsCRM {
     //Login
 
-    String logoAnTester = "//img[@alt='Perfex CRM | Anh Tester Demo']";
+
     String inputEmail = "//input[@id='email']";
     String inputPassWord = "//input[@id='password']";
     String logoLogin = "//h1[normalize-space()='Login']";
-    String labelEmail = "//label[normalize-space()='Email Address']";
-    String labelPassword = "//label[normalize-space()='Password']";
-    String labelRememberMe = "//label[normalize-space()='Remember me']";
+
     String checkboxRememberMe = "//input[@id='remember']";
     String buttonLogin = "//button[normalize-space()='Login']";
     String linkForgotPassword = "//a[normalize-space()='Forgot Password?']";
+    String alertErrorMessage = "//div[contains(text(), 'Invalid email')]";
+    String alertErrorMessageEmailRequired = "//div[text()='The Email Address field is required.']";
+    String alertErrorMessagePasswordRequired = "//div[text()='The Password field is required.']";
 
     //Menu Leads
     String  menuLeads = "//span[@class='menu-text' and normalize-space()='Leads']";
@@ -22,26 +23,26 @@ public class LocatorsCRM {
     // Xpath Button of Leads
 
     String buttonNewLeads = "//a[normalize-space()='New Lead']";
-    String buttonLeadsSummary = "//a[@class='btn btn-default btn-with-tooltip']";
-    String buttonKanban = "//a[@class='btn btn-default mleft5 hidden-xs']";
+    String buttonLeadsSummary = "//a[normalize-space()='Leads Summary']";
+    String buttonKanban = "//a[@data-title='Switch to Kanban']";
     String buttonFilterBy = "//button[normalize-space()='Filter by']";
-    String buttoExport = "//button[@class='btn btn-default buttons-collection btn-sm btn-default-dt-options']";
-    String buttonBulkActions = "//button[@class='btn btn-default btn-sm btn-default-dt-options']";
+    String buttoExport = "//button[normalize-space()='Export']";
+    String buttonBulkActions =  "//button[normalize-space()='Bulk Actions']";
     String buttonReload = "//button[@data-original-title='Reload']";
     String dropdownLength = "//select[@name='leads_length']";
 
-    String inputSearch = "//input[@class='form-control input-sm']";
+    String inputSearch =  "//div[@id='leads_filter']//input[@type='search']";
 
 
     //Header
     String titleLeadsSummary = "//h4[normalize-space()='Leads Summary']";
-    String titleActive = "//span[normalize-space()='Active']";
-    String titlejjjj = "//span[normalize-space()='jjjj']";
-    String titleCutomes = "//div[contains(@class,'tw-items-center')]//span[contains(text(),'Customer')]";
-    String titleLostLeads = "//span[@class='text-danger']";
+    String titleActive = "//div[contains(@class,'leads-overview')]//span[normalize-space()='Active']";
+    String numberValueActive = "//div[contains(@class,'leads-overview')]//span[normalize-space() = 'Active']/preceding-sibling::span";
+    String titleCutomes = "//div[contains(@class,'leads-overview')]//span[normalize-space() = 'Customer']";
+    String numberValueCustomer = "//div[contains(@class,'leads-overview')]//span[normalize-space() = 'Customer']/preceding-sibling::span";
 
     //Xpath table
-     String checkboxAll = "//div[@class='checkbox mass_select_all_wrap']";
+     String checkboxAll = "//input[@id='mass_select_all']";
      String labelSTT = "//th[@id='th-number']";
      String labelName = "//th[@id='th-name']";
      String labelCompany = "//th[@id='th-company']";
@@ -55,43 +56,62 @@ public class LocatorsCRM {
      String labelLastContact= "//th[@id='th-last-contact']";
      String labelCreated = "//th[@id='th-date-created']";
 
+    public static String buttonView(String leadName){
+        String buttonView = "//td[./a[normalize-space()='" + leadName +"']]//a[normalize-space()='View']";
+        return buttonView;
+    }
+
+    public static String buttonEdit(String leadName){
+        String buttonEdit = "//td[./a[normalize-space()='" + leadName +"']]//a[normalize-space()='Edit']";
+        return buttonEdit;
+    }
+
+    public static String buttonDelete(String leadName){
+        String buttonDelete = "//td[./a[normalize-space()='" + leadName +"']]//a[normalize-space()='Delete']";
+        return buttonDelete;
+    }
+
+
     // ====== Add New Lead Form ======
+    String btnAddLead = "//a[normalize-space()='New Lead']";
+    String titleAddNewLead = "//h4[normalize-space() ='Add new lead']";
 
      String labelStatusAdd = "//label[@for='status']";
-     String requiredStatusMark = "//label[@for='status']//small[@class='req text-danger'][normalize-space()='*']";
-     String dropdownStatusButton = "//button[@data-id='status']";
      String inputStatusSearch = "//button[@data-id='status']/following-sibling::div//input";
-     String dropdownStatusOption = "//div[@id='bs-select-5']//a[@id='bs-select-5-3']/span";
-     String buttonAddNewStatus = "//a[@onclick='new_lead_status_inline();return false;']";
+    public static String ListStatus(String value){
+        String ListStatus = "//button[@data-id='status']/following-sibling::div//span[normalize-space()='"+value+"']";
+        return ListStatus;
+    }
 
-     String requiredSource = "//label[@for='source']//small[@class='req text-danger'][normalize-space()='*']";
+
      String labelSourceAdd = "//label[@for='source']";
      String dropdownSourceButton = "//button[@data-id='source']";
      String inputSourceSearch = "//button[@data-id='source']/following-sibling::div//input";
-     String dropdownSourceOption = "//a[@id='bs-select-6-2']/span";
-     String buttonAddNewSource = "//a[@onclick='new_lead_source_inline();return false;']";
+    public static String ListSource(String value){
+        String ListSource = "//button[@data-id='source']/following-sibling::div//span[normalize-space()='"+value+"']";
+        return ListSource;
+    }
 
 
      String labelAssignedAdd = "//label[@for='assigned']";
      String dropdownAssignedButton = "//button[@title='Admin Example']";
      String inputAssignedSearch = "//div[@class='dropdown bootstrap-select bs3 open']//input[@aria-label='Search']";
-     String dropdownAssignedOption = "//div[@id='bs-select-7']//span[normalize-space()='Admin Anh Tester']";
-
+    public static String ListAssigned(String value){
+        String ListAssignes = "//button[@data-id='assigned']/following-sibling::div//span[normalize-space()='" + value + "']";
+        return ListAssignes;
+    }
 
      String labelTag = "//label[normalize-space()='Tags']";
      String inputTag = "//div[@id='inputTagsWrapper']//input[@placeholder='Tag']";
+    public static String ListTags(String value){
+        String ListTags = "//div[@id='inputTagsWrapper']//div[normalize-space()='" + value + "']";
+        return ListTags;
+    }
 
 
-     String labelNameAdd = "//div[@class='col-md-6']//label[@for='name']";
-     String requiredName = "///div[@class='col-md-6']//small[@class='req text-danger'][normalize-space()='*']";
-     String inputName = "//div[@class='col-md-6']//input[@id='name']";
-
-     String labelAddress = "//label[normalize-space()='Address']";
+     String inputName = "//div[@id ='tab_lead_profile']//input[@id='name']";
      String inputAddress = "//textarea[@id='address']";
-
-     String labelPosition = "//label[normalize-space()='Position']";
      String inputPosition = "//input[@id='title']";
-
      String labelCity = "//label[normalize-space()='City']";
      String inputCity = "//input[@id='city']";
 
@@ -105,8 +125,10 @@ public class LocatorsCRM {
      String inputWebsite = "//input[@id='website']";
 
      String labelCountry = "//label[normalize-space()='Country']";
-     String dropdownCountry = "//div[@app-field-wrapper='country']//button[@title='Nothing selected']']";
-
+    public static String ListCountry(String value) {
+        String ListCountry = "//button[@data-id='country']/following::span[@class='text' and normalize-space()='"+value+"']";
+        return ListCountry;
+    }
 
      String labelPhoneAdd = "//label[normalize-space()='Phone']";
      String inputPhone = "//input[@id='phonenumber']";
@@ -114,13 +136,16 @@ public class LocatorsCRM {
      String labelZipCode = "//label[normalize-space()='Zip Code']";
      String inputZipCode = "//input[@id='zip']";
 
-     String labelLeadValue = "//label[normalize-space()='Lead value']";
+
      String inputLeadValue = "//input[@name='lead_value']";
      String iconLeadValue = "//div[contains(text(),'$')]";
 
      String labelDefaultLanguage = "//label[normalize-space()='Default Language']";
      String dropdownDefaultLanguage = "//button[@data-id='default_language']";
-
+    public static String ListDefaultLanguage(String value){
+        String ListDefaultLanguage = "//button[@data-id='default_language']/following::span[@class='text' and normalize-space()='"+value+"']";
+        return ListDefaultLanguage;
+    }
 
      String labelCompanyAdd = "//label[normalize-space()='Company']";
      String inputCompany = "//textarea[@id='company']";
@@ -128,20 +153,13 @@ public class LocatorsCRM {
      String labelDescription = "//label[normalize-space()='Description']";
      String inputDescription = "//input[@id='description']";
 
-
-
-     String labelCheckboxPublic = "//label[@for='lead_public']";
      String checkboxPublic = "//input[@id='lead_public']";
 
-
-     String labelCheckboxContactedToday = "//label[normalize-space()='Contacted Today']";
      String checkboxContactedToday = "//input[@id='contacted_today']";
-
-     String labelDateContacted = "//label[normalize-space()='Date Contacted']";
      String inputDateContacted = "//input[@id='custom_contact_date']";
 
 
-     String buttonClose = "//button[@type=' button']";            // Close
+     String buttonClose = "//div[@id ='tab_lead_profile']//button[normalize-space()='Close']";           // Close
      String buttonSave = "//button[@id='lead-form-submit']";              // Save
 
 
